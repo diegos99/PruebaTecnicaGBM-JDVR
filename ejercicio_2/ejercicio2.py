647b7696a0327439ea958071d7d81c1b0acdacd2
diegos99 <diegovr99@hotmail.com>
2021-05-26 18:46:17 -0600

    ejercicio2 without optimizations

---
+++ ejercicio_2/ejercicio2.py
@@ -0,0 +1,79 @@
+import random
+
+G = 3 # Total gran premios
+pilotos_id = [32,24,35,54,76,23,87,95,15,18,14,65,78,66,90] # Identificador unico de cada piloto
+P = len(pilotos_id) # Total pilotos
+SP1 = [[1,2,3,4,5,6,7,8], [10,8,6,5,4,3,2,1]] # Sistema de puntaje 1
+SP2 = [[1,2,3,4,5,6,7,8,9,10], [25,18,15,12,10,8,6,4,2,1]] # Sistema de puntaje 2
+puntos_por_carrera = []
+
+# Inputs
+print("----------------------INPUTS----------------------")
+print("Total gran premios: ", G)
+print("Total pilotos es: ", P)
+print("Id's. Pilotos participantes: ", pilotos_id, "\n")
+print("Sistema de puntajes 1: ", SP1[0], "\n                       ", SP1[1])
+print("Sistema de puntajes 2: ", SP2[0], "\n                       ", SP2[1])
+
+def AsignarPunteoSP1(posicion):
+    if posicion > 8:
+        return 0
+    else:
+        return SP1[1][posicion-1]
+
+def AsignarPunteoSP2(posicion):
+    if posicion > 10:
+        return 0
+    else:
+        return SP2[1][posicion-1]
+
+# Ciclo For para simular cada carrera/gran premio
+for gran_premio in range(1,G+1):
+    print("GRAN PREMIO NO.",gran_premio)
+    parrilla = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] # Dado a que son 15 pilotos, hay 15 posiciones en la parrilla inicial.
+    resultados_gran_premio = [] # Almacena las posiciones de la carrera
+
+    # Ciclo For para simular la posicion obtenida de cada piloto al finalizar la carrera y 
+    # asignarle punteo en cada tipo de sistema de puntuación
+    for piloto in range(P):
+        posicion_obtenida = random.choice(parrilla)
+        print('Piloto:', pilotos_id[piloto],'Posicion', posicion_obtenida)
+        if gran_premio > 1:
+            puntos_por_carrera[0][piloto][1] = puntos_por_carrera[0][piloto][1] + AsignarPunteoSP1(posicion_obtenida)
+            puntos_por_carrera[0][piloto][2] = puntos_por_carrera[0][piloto][2] + AsignarPunteoSP2(posicion_obtenida)
+        else:
+            resultados_gran_premio.append([pilotos_id[piloto], AsignarPunteoSP1(posicion_obtenida), AsignarPunteoSP2(posicion_obtenida)])
+            puntos_por_carrera.append(resultados_gran_premio)
+        parrilla.remove(posicion_obtenida)
+
+# OUTPUTS
+# Muestra en consola los punteos finales de cada piloto. La primera columna detalla el id del piloto, 
+# la 2da columna muestra el punteo final en el Sistema de punteo 1, y la 3ra columna muestra el punteo final en el sistema de punteo 2.
+print("\n----------------------OUTPUTS----------------------")
+def VisualizarPunteosFinales():
+    print("PUNTEOS AL FINAL DE TEMPORADA (sistema de puntuación 1 y 2)")
+    for item in range(P):
+        print(puntos_por_carrera[0][item])
+
+VisualizarPunteosFinales()
+
+# Campeon(es) mundiales Sistema de puntuación 1 y 2
+def ResultadosSP12(sistema_p):
+    resultados_finales = []
+    ganadores = []
+    if sistema_p == 1:
+        print("CAMPEON(ES) DEL MUNDO SISTEMA PUNTUACIÓN", sistema_p)
+    elif sistema_p == 2:
+        print("CAMPEON(ES) DEL MUNDO SISTEMA PUNTUACIÓN", sistema_p)
+    else:
+        print("SISTEMA DE PUNTUACION NO VALIDO")
+    for item in range(P):
+        resultados_finales.append(puntos_por_carrera[0][item][sistema_p])
+    valor_max = max(resultados_finales)
+    for item in range(P):
+        if puntos_por_carrera[0][item][sistema_p] == valor_max:
+            ganadores.append(puntos_por_carrera[0][item][0])
+    print(ganadores)
+
+ResultadosSP12(1) # Sistema puntuación 1
+ResultadosSP12(2) # Sistema puntuación 2
